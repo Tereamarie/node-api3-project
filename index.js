e30c26deb1720ac1c7394dc37c7d3bd4e3a89a43
marie-hotchkiss <mtknowlton67@gmail.com>
2020-07-15 16:42:45 -0700

    First commit created server.js/index.js set server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code away!
+const express = require("express");
+
+
+const server = express();
+
+
+server.get('/', (req, res) => {
+    
+    res.send(`
+    <h2>Lambda Hubs</h2>
+    <p>Welcome${nameInsert} to the Lambda</p>`)
+})
+server.listen(4000, () => {
+    console.log("\n* Server Running on http://localhost:4000 *\n");
+  });
+  
